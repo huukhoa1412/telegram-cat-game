b05448cd8526f656bb42f99f01e1201e32052a80
MonPham <demondhunter93@gmail.com>
2024-04-25 23:50:54 +0800

    .

--- generateUrl.js
+++ generateUrl.js
@@ -1,30 +1,15 @@
-function generateUrl() {
-    // Extract data from localStorage
-    // let localStorageData = JSON.parse(localStorage.getItem("CAT_LOGINDATA_1"));
-    // let token = localStorageData?.token;
+function convertObjectToURLLink(obj) {
+    const baseLink = "https://tgsvr.catizen.ai/api/bot/tmas/gameapp/catizenbot";
+    const startParam = obj.start_param;
+    const userData = encodeURIComponent(JSON.stringify(obj.user));
+    const chatInstance = obj.chat_instance;
+    const chatType = obj.chat_type;
+    const authDate = obj.auth_date;
+    const hash = obj.hash;
+    
+    const queryParams = `tgWebAppStartParam=${startParam}#tgWebAppData=user%3D${userData}%26chat_instance%3D${chatInstance}%26chat_type%3D${chatType}%26start_param%3D${startParam}%26auth_date%3D${authDate}%26hash%3D${hash}&tgWebAppVersion=7.2&tgWebAppPlatform=weba&tgWebAppBotInline=1&tgWebAppThemeParams=%7B%22bg_color%22%3A%22%23212121%22%2C%22text_color%22%3A%22%23ffffff%22%2C%22hint_color%22%3A%22%23aaaaaa%22%2C%22link_color%22%3A%22%238774e1%22%2C%22button_color%22%3A%22%238774e1%22%2C%22button_text_color%22%3A%22%23ffffff%22%2C%22secondary_bg_color%22%3A%22%230f0f0f%22%2C%22header_bg_color%22%3A%22%23212121%22%2C%22accent_text_color%22%3A%22%238774e1%22%2C%22section_bg_color%22%3A%22%23212121%22%2C%22section_header_text_color%22%3A%22%23aaaaaa%22%2C%22subtitle_text_color%22%3A%22%23aaaaaa%22%2C%22destructive_text_color%22%3A%22%23e53935%22%7D`;
 
-    // Extract data from sessionStorage
-    let sessionStorageData = JSON.parse(sessionStorage.getItem("__telegram__initParams"));
-    let tgWebAppData = encodeURIComponent(sessionStorageData.tgWebAppData);
-    let tgWebAppVersion = sessionStorageData.tgWebAppVersion;
-    let tgWebAppPlatform = sessionStorageData.tgWebAppPlatform;
-    let tgWebAppBotInline = sessionStorageData.tgWebAppBotInline;
-    let tgWebAppThemeParams = encodeURIComponent(sessionStorageData.tgWebAppThemeParams);
-
-    // Extract data from Telegram.WebApp.initDataUnsafe
-    let telegramData = Telegram.WebApp.initDataUnsafe;
-    let query_id = telegramData.query_id;
-    let user = encodeURIComponent(JSON.stringify(telegramData.user));
-    let auth_date = telegramData.auth_date;
-    let hash = telegramData.hash;
-
-    // Generate URL  
-    //https://gs1.catizen.ai:8882/api/bot/gameapplogin?
-    // let url = `https://game.catizen.ai/tgCat/game/catizen240403.html?tok`;
-    //let url = `https://tgsvr.catizen.ai/api/bot/tmas/gameapp/catizenbot#tgWebAppData=query_id%3D${query_id}%26user%3D${user}%26auth_date%3D${auth_date}%26hash%3D${hash}&tgWebAppVersion=${tgWebAppVersion}&tgWebAppPlatform=${tgWebAppPlatform}&tgWebAppBotInline=${tgWebAppBotInline}&tgWebAppThemeParams=${tgWebAppThemeParams}`;
-    //https://game.catizen.ai/tgCat/game/mantle.html?ts=1714056777246&botname=Catizen_Mntbot
-    let url = `https://game.catizen.ai/tgCat/game/mantle.html?ts=1714056777246&botname=Catizen_Mntbot#tgWebAppData=query_id%3D${query_id}%26user%3D${user}%26auth_date%3D${auth_date}%26hash%3D${hash}&tgWebAppVersion=${tgWebAppVersion}&tgWebAppPlatform=${tgWebAppPlatform}&tgWebAppBotInline=${tgWebAppBotInline}&tgWebAppThemeParams=${tgWebAppThemeParams}`;
-    //https://tgsvr.catizen.ai/api/bot/gameapplogin?botname=catizen
-    return url;
+    return `${baseLink}?${queryParams}`;
 }
-generateUrl()
+const urlLink = convertObjectToURLLink(Telegram.WebApp.initDataUnsafe);
+console.log(urlLink);
