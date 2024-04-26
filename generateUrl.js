d93644e94470e9faa97a9790f2ba801962891f05
MonPham <demondhunter93@gmail.com>
2024-04-26 13:04:23 +0800

    .

--- generateUrl.js
+++ generateUrl.js
@@ -8,13 +8,13 @@ function convertSessionStorageToURLLink() {
     }
 
     const initData = JSON.parse(sessionStorageData);
-    const baseLink = "https://tgsvr.catizen.ai/api/bot/gameapplogin";
+    const baseLink = "https://tgsvr.catizen.ai/api/bot/tmas/gameapp/catizenbot";
     const botname = "catizenbot";
     const tguserid = Telegram.WebApp.initDataUnsafe.user.id;
     const tgusername = Telegram.WebApp.initDataUnsafe.user.username;
     const ts = initData.auth_date;
     const sign = initData.hash;
-    const queryData = encodeURIComponent(JSON.stringify(initData.tgWebAppData));
+    const queryData = encodeURIComponent((initData.tgWebAppData));
 
     const queryParams = `botname=${botname}&tguserid=${tguserid}&tgusername=${tgusername}&ts=${ts}&sign=${sign}#tgWebAppData=${queryData}&tgWebAppVersion=${initData.tgWebAppVersion}&tgWebAppPlatform=${initData.tgWebAppPlatform}&tgWebAppBotInline=${initData.tgWebAppBotInline}&tgWebAppThemeParams=${encodeURIComponent(initData.tgWebAppThemeParams)}`;
 
